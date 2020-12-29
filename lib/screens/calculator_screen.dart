a353b8b791f3e38f1d6e88d74705b02fbdb905be
danylo-macelai <danylomacelai@gmail.com>
2020-12-29 10:01:24 -0300

    Added assets retro_calculator

---
+++ lib/screens/calculator_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:retro_calculator/utils/constant.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(12.0),
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                mBackgroundColor,
+                mPrimaryColor,
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: Column(
+            children: [
+              Text('Display'),
+              Text('Keyboard'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const mPrimaryColor = Color(0xFFF9EDD3);
+const mAccentColor = Color(0xFF707070);
+const mBackgroundColor = Color(0xFFEFDFBB);
